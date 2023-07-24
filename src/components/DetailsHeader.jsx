cb808a396da99f66116c0214e643d320e94cd691
b1tburn3r20 <alejandrojosuebermudez@gmail.com>
2023-07-24 12:28:23 -0500

    Artist Page 404, added GEOLOCATION

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -4,18 +4,16 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
   const artist = artistData?.artists[artistId]?.attributes;
   return (
     <div className="relative w-full flex flex-col">
-      <div className="w-full bg-gradient-to-l from-transparent to-black sm: h-48 h-28" />
+      <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
       <div className="absolute inset-0 flex items-center">
         <img
           src={
             artistId
-              ? artistData?.artist.artwork?.url
-                  .replace("{w}", "500")
-                  .replace("{h}", "500")
+              ? artist?.artwork?.url.replace("{w}", "500").replace("{h}", "500")
               : songData?.images?.coverart
           }
           alt="art"
-          className="sm: w-48 w-28 sm: h-48 h-28 rounded-full object-conver border-2 shadow-xl shadow-black"
+          className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
         />
         <div className="ml-5">
           <p className="font-bold sm:text-3xl text-xl text-white">
@@ -27,7 +25,7 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
                 </p>
               </Link>
             )}
-            <p className="text-base  text-gray-400 mt-2">
+            <p className="text-base text-gray-400 mt-2">
               {artistId ? artist?.genreNames[0] : songData?.genres.primary}
             </p>
           </p>
